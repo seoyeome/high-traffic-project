74bd7200c739d283cdd3e2615a797a17052bd615
SeoYoung <seoyeomedev@gmail.com>
2025-09-21 22:59:05 +0900

    [refactor] Serial 가시화

--- src/main/java/org/example/hightrafficproject/entity/Product.java
+++ src/main/java/org/example/hightrafficproject/entity/Product.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.io.Serial;
 import java.io.Serializable;
 
 @Entity
@@ -14,6 +15,7 @@ import java.io.Serializable;
 @AllArgsConstructor
 @Builder
 public class Product implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
